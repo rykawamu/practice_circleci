873407f2f0b98b609c597b676d26bbd786a44841
rykawamu <ryu2.endless.waltz@gmail.com>
2019-03-05 20:02:33 +0900

    テストエラーを回避＋Buzzの判定ロジックを追加

--- src/fizzbuzz/fizzbuzz.go
+++ src/fizzbuzz/fizzbuzz.go
@@ -4,8 +4,16 @@ import (
 //    "fmt"
 )
 
+// Is Fizz is check function.
 func IsFizz(val int) bool {
-    //isFizz := val % 3 == 0
-    isFizz := true
+    isFizz := val % 3 == 0
+    //isFizz := true
     return isFizz
+}
+
+// Is Buzz is check function.
+func IsBuzz(val int) bool {
+    isBuzz := val % 5 == 0
+    //isBuzz := true
+    return isBuzz
 }
--- src/fizzbuzz/fizzbuzz_test.go
+++ src/fizzbuzz/fizzbuzz_test.go
@@ -10,14 +10,44 @@ func Test_IsFizz(t *testing.T) {
         in  int
         out bool
     }{
-        {name:"Fizz False", in: 3, out: false,},
+        {name:"Fizz False", in: 1, out: false,},
         {name:"Fizz True", in: 3, out: true,},
+        {name:"Fizz False", in: 5, out: false,},
+        {name:"Fizz True", in: 6, out: true,},
+        {name:"Fizz False", in: 10, out: false,},
+        {name:"Fizz True", in: 15, out: true,},
     }
 
     for _, tt := range cases {
        st := tt
        t.Run(st.name, func(t *testing.T){
            s := IsFizz(st.in)
+           if s != st.out {
+               t.Fatal("出力値が異なる[", st.in, "]=>[", s, "] [" , st.out , "]")
+           }
+       })
+    }
+}
+
+func Test_IsBuzz(t *testing.T) {
+
+    cases := []struct {
+        name string
+        in  int
+        out bool
+    }{
+        {name:"Fizz False", in: 1, out: false,},
+        {name:"Fizz False", in: 3, out: false,},
+        {name:"Fizz True", in: 5, out: true,},
+        {name:"Fizz False", in: 6, out: false,},
+        {name:"Fizz True", in: 10, out: true,},
+        {name:"Fizz True", in: 15, out: true,},
+    }
+
+    for _, tt := range cases {
+       st := tt
+       t.Run(st.name, func(t *testing.T){
+           s := IsBuzz(st.in)
            if s != st.out {
                t.Fatal("出力値が異なる[", st.in, "]=>[", s, "] [" , st.out , "]")
            }
